29ed47ab376dc4a84ad9a0af37a2a9cd4d8b10c3
granogueira <grazy_nog@hotmail.com>
2024-02-14 16:58:59 -0300

    projeto

---
+++ app.js
@@ -0,0 +1,10 @@
+function exibirTextoNaTela(tag, texto) {
+let campo = document.querySelector('tag');
+paragrafo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1','Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function verificarchute() {
+        console.log ('o botão foi clicado')
+}    
